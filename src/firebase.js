551585a8b05f1edb3f7d9fba21581f893c8cd36e
Keisha Harris <keishadharriss@gmail.com>
2021-10-25 20:21:13 -0400

    Inital Commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAUKmzhqBNdH9Es_Xew3TbHmMn5wF1jv1k",
+    authDomain: "clone-832bf.firebaseapp.com",
+    projectId: "clone-832bf",
+    storageBucket: "clone-832bf.appspot.com",
+    messagingSenderId: "473550239362",
+    appId: "1:473550239362:web:7ebd23e6fa72d3dad8e69b",
+    measurementId: "G-K4WMRH275L"
+  };
